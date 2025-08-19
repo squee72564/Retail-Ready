2c1e881b38a4502f0ec37da4f762e73bcdac5e43
Alex Rodriguez <rodriguez.al183@gmail.com>
2025-08-18 20:45:08 -0700

    Update types for TS linting

--- frontend/types/next-auth.d.ts
+++ frontend/types/next-auth.d.ts
@@ -7,13 +7,19 @@ declare module "next-auth" {
     idToken?: string;
     realmRoles: string[];
     resourceRoles: Record<string, { roles: string[] }>;
+    error?: string;
   }
 }
 
 declare module "next-auth/jwt" {
-  interface JWT {
-    refreshToken?: string;
-    accessToken?: string;
-    idToken?: string;
+  export interface JWT {
+    realmRoles?: string[];
+    resourceRoles?: Record<string, { roles: string[] }>;
+    refreshToken: string;
+    accessToken: string;
+    idToken: string;
+    expiresAt: number;
+    refreshTokenExpiresAt?: number;
+    error?: string;
   }
 }
